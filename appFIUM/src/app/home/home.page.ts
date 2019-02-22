d0a1ebf99f4d2cc38af1ce37eae316f983611200
alejandro-nbk <alejandro.martinez@nubika.com>
2019-02-22 14:17:37 +0100

    Primeros pasos

--- appFIUM/src/app/home/home.page.ts
+++ appFIUM/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  user= { nombre: '', password: ''};
+
+  constructor(public navCtrl : NavController) {
+
+  }
+
+  showRegistroPage(){
+    console.log('registro');
+    this.navCtrl.navigateForward('/registro');
+  }
+
+  login_user(){
+    console.log('login');
+  }
 
 }
